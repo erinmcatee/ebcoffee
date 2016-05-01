2ccfaf96d00a7bd4fa41b1b498aea577298ece04
erinmcatee <mcateee@gmail.com>
2016-05-01 00:30:34 -0500

    setup child theme for ebcoffee

---
+++ wp-content/themes/x-child/framework/views/global/_cta.php
@@ -0,0 +1,3 @@
+<div class="cta">
+	<?php echo do_shortcode( '[button type="real" shape="rounded" size="large" href="#find-us" title="Find Us"][icon type="map-marker"]Find Us[/button]' ); ?>
+</div>
