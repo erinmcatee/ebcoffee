55815bf3af19623f5f0f5d6972fda8aada1885db
erinmcatee <mcateee@gmail.com>
2016-05-11 13:13:35 -0500

    add ga

--- wp-content/themes/x-child/functions.php
+++ wp-content/themes/x-child/functions.php
@@ -20,6 +20,29 @@ add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 
 
 
-// Additional Functions
+// Google Analytics
 // =============================================================================
+
+// Include the Google Analytics Tracking Code (ga.js)
+// @ https://developers.google.com/analytics/devguides/collection/gajs/
+function google_analytics_tracking_code(){ ?>
+
+		<script>
+		
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-77591609-1', 'auto');
+		  ga('send', 'pageview');
+		
+		</script>
+		
+<?php
+}
+
+// include GA tracking code before the closing head tag
+add_action('wp_head', 'google_analytics_tracking_code');
+
 
