4304cb460132ba172ec128dee4dd09d556ab1b4d
erinmcatee <mcateee@gmail.com>
2016-06-28 10:04:50 -0500

    fix find-us btn

--- wp-content/themes/x-child/framework/views/global/_cta.php
+++ wp-content/themes/x-child/framework/views/global/_cta.php
@@ -1,3 +1,3 @@
 <div class="cta x-column x-1-3 cs-ta-right">
-	<?php echo do_shortcode( '[button type="real" shape="rounded" size="large" href="#find-us" title="Find Us"][icon type="map-marker"]Find Us[/button]' ); ?>
+	<?php echo do_shortcode( '[button type="real" shape="rounded" size="large" href="'. home_url( '/' ) .'#find-us" title="Find Us"][icon type="map-marker"]Find Us[/button]' ); ?>
 </div>
