0ef603c7e39815c575cb9fb146ae8ac17adbd133
erinmcatee <mcateee@gmail.com>
2018-12-09 22:55:15 -0600

    update roast meta for merch & gifts

--- wp-content/themes/x-child/woocommerce/single-product/roast-meta.php
+++ wp-content/themes/x-child/woocommerce/single-product/roast-meta.php
@@ -15,33 +15,36 @@ $roast_value = get_field('roast_level');
 $notes = get_field('tasting_notes');
 $processing = get_field('processing');
 
+
+if ( get_field('roast_level') && get_field('roast_level') != 'not_roast') :
 ?>
 
-<table class="roast-meta">
-<?php 
-if( get_field('roast_level') ): ?>
-	<tr>
-			<td><span class="w-600"><?php _e( 'Roast Level', '__x__' ); ?></span></td>
-			<td>
-				<div class="fit-content">
-					<span class="roast-level small"><?php echo do_shortcode( '[roast level='.$roast_value.']' ); ?></span><div class="roast-light"><?php _e( 'Light', '__x__' ); ?></div><div class="roast-dark"><?php _e( 'Dark', '__x__' ); ?></div>
-				</div>
-			</td>
-	</tr>
-<?php endif; 
-
-if( get_field('tasting_notes') ): ?>
-	<tr>
-		<td><span class="w-600"><?php _e( 'Tasting Notes', '__x__' ); ?></span></td>
-		<td><span class="small"><?php echo $notes; ?></span></td>
-	</tr>
-<?php endif;
-
-if( get_field('processing') ): ?>
-	<tr>
-		<td><span class="w-600"><?php _e( 'Processing', '__x__' ); ?></span></td>
-		<td><span class="small"><?php echo $processing; ?></span></td>
-	</tr>
+	<table class="roast-meta">
+	<?php 
+	if( get_field('roast_level') ): ?>
+		<tr>
+				<td><span class="w-600"><?php _e( 'Roast Level', '__x__' ); ?></span></td>
+				<td>
+					<div class="fit-content">
+						<span class="roast-level small"><?php echo do_shortcode( '[roast level='.$roast_value.']' ); ?></span><div class="roast-light"><?php _e( 'Light', '__x__' ); ?></div><div class="roast-dark"><?php _e( 'Dark', '__x__' ); ?></div>
+					</div>
+				</td>
+		</tr>
+	<?php endif; 
+	
+	if( get_field('tasting_notes') ): ?>
+		<tr>
+			<td><span class="w-600"><?php _e( 'Tasting Notes', '__x__' ); ?></span></td>
+			<td><span class="small"><?php echo $notes; ?></span></td>
+		</tr>
+	<?php endif;
+	
+	if( get_field('processing') ): ?>
+		<tr>
+			<td><span class="w-600"><?php _e( 'Processing', '__x__' ); ?></span></td>
+			<td><span class="small"><?php echo $processing; ?></span></td>
+		</tr>
+	<?php endif; ?>
+	
+	</table>
 <?php endif; ?>
-
-</table>
