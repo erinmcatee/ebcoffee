f0624d77a9d78f10f9c2f5dfa59c117e67b0633b
erinmcatee <mcateee@gmail.com>
2020-05-25 23:50:11 -0500

    fix checkout breaking bug

--- wp-content/themes/x-child/woocommerce/checkout/payment.php
+++ wp-content/themes/x-child/woocommerce/checkout/payment.php
@@ -30,7 +30,7 @@ if ( ! is_ajax() ) {
 						wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
 					}
 				} else {
-					echo '<li>' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html __( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>';
+					echo '<li>' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>';
 				}
 			?>
 		</ul>
